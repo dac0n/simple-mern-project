0ea99b4ea676abc40e483afee2b96b023262be87
dac0n <dac0noid@gmail.com>
2021-02-04 15:38:22 +0200

    changed gitignore

--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,7 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
+  console.log('fffffffffffffffffffffffff', error);
   if (req.file){
     fs.unlink(req.file.path, err => {
       console.log(err);
