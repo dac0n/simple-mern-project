27611d7227ec6254683b7a3299eed3402628a4ea
dac0n <dac0noid@gmail.com>
2021-02-04 18:33:25 +0200

    test

--- backend/middleware/file-upload.js
+++ backend/middleware/file-upload.js
@@ -14,8 +14,8 @@ const fileUpload = multer({
     },
     filename: (req, file, cb) => {
       const ext = MIME_TYPE_MAP[file.mimetype];
-      cb(null, uuid() + "." + ext);
-    },
+      cb(null, uuid() + '.' + ext);
+    }
   }),
   fileFilter: (req, file, cb) => {
     const isValid = !!MIME_TYPE_MAP[file.mimetype];  
